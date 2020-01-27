bc0a9c6c862c881b04b0d8888c38c6cb8076b7e3
github username <github email address>
2020-01-27 17:36:05 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,9 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
+  
+
 
   def call(env)
     resp = Rack::Response.new
@@ -13,9 +16,27 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+      elsif req.path.match(/add/)
+        item = req.params["item"]
+        if @@items.include?(item)
+          @@cart << item
+          resp.write "added #{item}"
+        else
+          resp.write "We don't have that item"
+        end
     else
-      resp.write "Path Not Found"
+      resp.write "Path not found"
     end
+
+     
 
     resp.finish
   end
@@ -27,4 +48,12 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+
+
+  
+
+
+
 end
+
---
+++ new.rb
@@ -0,0 +1,35 @@
+class Application
+
+    @@items = ["Apples","Carrots","Pears"]
+    @@cart = []
+    @@cart << @@items
+  
+    def call(env)
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
+  
+      if req.path.match(/items/)
+        @@items.each do |item|
+          resp.write "#{item}\n"
+        end
+      elsif req.path.match(/search/)
+        search_term = req.params["q"]
+        resp.write handle_search(search_term)
+      else
+        resp.write "Path Not Found"
+      end
+  
+      resp.finish
+    end
+  
+    def handle_search(search_term)
+      if @@items.include?(search_term)
+        return "#{search_term} is one of our items"
+      else
+        return "Couldn't find #{search_term}"
+      end
+    end
+  
+  
+  end
+  
